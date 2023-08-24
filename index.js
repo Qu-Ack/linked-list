89086963386a8c6b01ec48b07b90258afd0ea919
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2023-08-24 11:46:38 +0530

    prepend , print head , print tail , print size

--- index.js
+++ index.js
@@ -17,15 +17,16 @@ const LinkedList = function () {
 
 
     function append(value) {
+        let node = new Node(value)
         if (head == null) {
-            head = new Node(value)
+            head = node
             size++;
         } else {
             let tmp = head;
             while(tmp.next != null) {
                 tmp = tmp.next; 
             }
-            tmp.next = new Node(value)
+            tmp.next = node
             size++
         }
 
@@ -35,23 +36,45 @@ const LinkedList = function () {
         let node = new Node(value); 
         if (head == null) {
             head = node;
+            
             size++
         } else {
-            let tmp  = head;
-            node.next = tmp;
-            node = head; 
-            console.log(head)
+            node.next = head;
+            head = node;
+            
             size++
         }   
     }
 
+    function printTail() {
+        let current = head;
+        while(current.next !== null) {
+            current = current.next
+        }
+        console.log(current.data);
+    }
 
-    return {append , prepend , printListsize}
+
+
+    function printHead() {
+        console.log(head)
+    }
+
+    function at(index) {
+        
+    }
+
+    return {append , prepend , printListsize , printHead , printTail}
 }
 let linkedList =  LinkedList();
 linkedList.append(3);
 linkedList.append(4);
 linkedList.append(5);
-linkedList.append(10)
 linkedList.prepend(2)
+linkedList.append(10)
+linkedList.append(12)
 linkedList.printListsize();
+linkedList.printHead();
+linkedList.printTail();
+
+
